a8dd86a52438da7b86cd338820710845814d3a5c
minomi <5minhominho@gmail.com>
2017-06-27 14:40:23 +0900

    Initial Commit

---
+++ MyMovieChart/MovieCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 오민호 on 2017. 6. 26..
+//  Copyright © 2017년 오민호. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell : UITableViewCell {
+    
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var opendate: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    
+}
