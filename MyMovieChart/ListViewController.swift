1513c45eb63f0e041b33722d40fbd98e3b8e8ee8
minomi <5minhominho@gmail.com>
2017-06-28 22:38:23 +0900

    api로 영화리스트 가져옴

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -8,27 +8,53 @@
 
 import UIKit
 class ListViewController : UITableViewController {
-    var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.","2008-09-04",8.95,"darknight.jpg"),
-        ("호우시절", "때를 알고 내리는 좋은 비","2009-10-08",7.31,"rain.jpg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07",9.9,"secret.jpg")
-    ]
     
+    @IBOutlet weak var moreBtn: UIButton!
+    var page = 1
     lazy var list : [MovieVO] = {
         var datalist = [MovieVO]()
-        for(title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            datalist.append(mvo)
-        }
         return datalist
     }()
     
+    func callMovieAPI() {
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI : URL! = URL(string: url)
+        let apidata = try! Data(contentsOf: apiURI)
+        
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "No Data"
+        NSLog("API Result = \(log)")
+        
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            for row in movie {
+                let r = row as! NSDictionary
+                let mvo = MovieVO()
+                
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = (r["ratingAverage"] as! NSString).doubleValue
+                
+                self.list.append(mvo)
+            }
+            
+            let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+            
+            if(self.list.count >= totalCount) {
+                self.moreBtn.isHidden = true
+            }
+        } catch {
+            NSLog("Parse Error!!")
+        }
+    }
+    
     override func viewDidLoad() {
+        self.callMovieAPI()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -45,12 +71,18 @@ class ListViewController : UITableViewController {
         cell.desc.text = row.description
         cell.opendate.text = row.opendate
         cell.rating.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        cell.thumbnail.image = UIImage(data: try! Data(contentsOf: URL(string: row.thumbnail!)!))
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은\(indexPath.row)번째 행입니다.")
+    }
+    
+    @IBAction func more(_ sender: UIButton) {
+        self.page += 1
+        callMovieAPI()
+        self.tableView.reloadData()
     }
 }
